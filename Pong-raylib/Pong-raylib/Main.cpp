6da3d0ab51a755936353e5feae7c5a60244d4707
mircomax <mircom0801@gmail.com>
2024-08-26 20:19:37 +0200

    Add files via upload
    Aggiunte funzioni importanti per incrementare la difficoltà e la velocità della palla man mano che il gioco va avanti, modificata la grafica e aggiunti elementi luminosi quando viene segnato un goal insieme alla scritta "GO!" che compare al centro dello schermo.

--- Pong-raylib/Pong-raylib/Main.cpp
+++ Pong-raylib/Pong-raylib/Main.cpp
@@ -2,20 +2,22 @@
 #include <random>
 #include <cstdlib>
 #include <ctime>
+#include <iostream>
+#include <cmath>
 class Ball {
     private:
         float x, y;
-        int speed_x, speed_y, radius;
+        float speed_x, speed_y, radius;
     public:
         Ball() { //costruttore in modo che ogni palla creata abbia gli stessi parametri
             x = 1280 / 2;
             y = 800 / 2;
-            speed_x = 7;
-            speed_y = 7;
-            radius = 20;
+            speed_x = -6.5;
+            speed_y = -6.5;
+            radius = 20.0;
         }
         void DrawBall() {
-            DrawCircle(x, y, radius, WHITE); //Disegna la palla
+            DrawCircle(x, y, radius, GetColor(0xFFD700FF)); //Disegna la palla
         }
         void UpdateBall() { //far muovere la palla
             x += speed_x;
@@ -28,31 +30,43 @@ class Ball {
             }
         }
         void ResetPartita() {
-            x = GetScreenWidth() / 2;
+            // Riposiziona la palla al centro
+            x = GetScreenWidth() / 2; 
             y = GetScreenHeight() / 2;
         } 
         Vector2 GetCD() {
             return Vector2{ x,y };
         }
+
         void InvertiVelocitaX() {
-            speed_x *= -1;
-            speed_x += 0.17;
-            
+             speed_x *= -1;
+
+             // Aumenta la velocità della palla per rendere il gioco più veloce
+             speed_x += (speed_x > 0) ? 0.15f : -0.15f;
+             speed_y += (speed_y > 0) ? 0.15f : -0.15f;
+
+             // Genera una variazione casuale nella velocità verticale
+             float variazione = (static_cast<float>(rand() % 41) - 20) / 100.0f;
+             speed_y += variazione;
         }
+
         int GetX() {
             return x;
         }
-        void setspeedY(int speedY) {
+        void setspeedY(float speedY) {
             speed_y = speedY;
+        }
+        void setspeedX(float speedX) {
+            speed_x = speedX;
         }
 };
 
 void CheckMovimentoPaddle(int &Paddle_y) { //paddle necessario per il movimento
     if (IsKeyDown(KEY_DOWN) && Paddle_y + 70 < GetScreenHeight()) {
-        Paddle_y += 7; //sposta il paddle verso il basso
+        Paddle_y += 6.5; //sposta il paddle verso il basso
     }
     else if (IsKeyDown(KEY_UP) && Paddle_y - 70 > 0) {
-        Paddle_y -= 7; //sposta il paddle verso l'alto
+        Paddle_y -= 6.5; //sposta il paddle verso l'alto
     }
 }
 
@@ -75,6 +89,45 @@ void MovimentoPaddleAI(float& PaddleAI_y, float PaddleAI_speed, Ball& palla) {
     }
 }
 
+float IncrementoDifficolta(int& PunteggioAI, int& PunteggioUser) {
+    if (PunteggioUser - PunteggioAI >= 2) {
+        return 0.5f;
+    }
+    else if (PunteggioUser - PunteggioAI == 1) {
+        return 0.2f;
+    }
+    else if (PunteggioAI > PunteggioUser) {
+        return -0.5f;
+    }
+    else {
+        return 0.0f;
+    }
+}
+
+void LightEffect(Rectangle rett, Color lightcolor, int showLightEffect, Ball& palla) {
+    if (showLightEffect > 1 && showLightEffect % 2 == 0) {
+        DrawRectangleRec(rett, lightcolor);
+    }
+    if (showLightEffect > 1) palla.ResetPartita(); // Ferma la palla al centro
+}
+
+void MostraTestoGo(bool& showGoText, int& goTextFrames, const int goTextDurationFrames, int showLightEffect) {
+    if (showLightEffect < 5) {// Mostriamo il testo solo quando la palla sta per ripartire
+        if (showGoText) {
+            // Disegna il testo al centro dello schermo
+            DrawText("GO!", (GetScreenWidth() / 2) - (MeasureText("GO!", 100) / 2), (GetScreenHeight() / 2) - (MeasureText("GO!", 100) / 2), 100, BLACK);
+
+            // Decrementa il contatore dei frame
+            goTextFrames--;
+
+            // Disattiva il flag quando il contatore dei frame raggiunge 0
+            if (goTextFrames <= 0) {
+                showGoText = false;
+            }
+        }
+    }
+}
+
 Ball palla1;
 int main(void)
 {
@@ -85,12 +138,27 @@ int main(void)
     const int screenHeight = 800;
     int Paddle_y = screenHeight / 2;
     float PaddleAI_y = screenHeight / 2;
-    float PaddleAI_speed = 5.85;
+    float PaddleAI_speed = 5.8;
     int PunteggioUser = 0, PunteggioAI = 0;
-    SetConfigFlags(FLAG_VSYNC_HINT);
+    SetConfigFlags(FLAG_VSYNC_HINT); // Rende tutto più fluido
     InitWindow(screenWidth, screenHeight, "Pong game!");
-    
+    int counter = 0;
     SetTargetFPS(60); // Set our game to run at 60 frames-per-second
+
+    //Colori
+    Color cornflowerBlue = GetColor(0x6495EDFF); // CornflowerBlue con alfa pieno
+    Color PaddleColor = LIGHTGRAY; // Colore paddle
+    Color TextColor = GetColor(0xFFD700FF); // Gold Color
+
+    //Variabili per gli effetti luminosi
+    Color lightColor = LIME; // Colore 
+    int showLightEffect = 0; //Essenziale per mostrare effetto luminoso
+    Rectangle lightRect = { 0, 0, 0, 0 }; // Rettangolo che verrà poi colorato
+
+    // Variabili per il testo "GO!"
+    bool showGoText = false;
+    int goTextFrames = 0;
+    const int goTextDurationFrames = 30; // Mostra il testo per 30 frame
 
     // Main game loop
     while (!WindowShouldClose())    // Detect window close button or ESC key
@@ -101,35 +169,73 @@ int main(void)
         Rectangle PaddleGiocatore = { (float)(screenWidth - 35), (float)(Paddle_y - 60), (float)25, (float)120 };
         Rectangle PaddleAI = {(float)10,(float)(PaddleAI_y - 60),(float)25,(float)120 };
         MovimentoPaddleAI(PaddleAI_y, PaddleAI_speed, palla1);
+
         //Controllo se uno dei due giocatori fa punto
-        if (palla1.GetX() + 20 >= screenWidth) {
+        if (palla1.GetX() + 20 >= screenWidth) { 
             palla1.ResetPartita(); //viene resettata la palla al centro dello schermo
             PunteggioAI++; //incremento punteggio
+
+            showGoText = true;
+            goTextFrames = goTextDurationFrames;
+
+            //Effetti illuminazione
+            showLightEffect = 30;
+            lightRect = { screenWidth - 50, 10, 45, screenHeight - 20 };
+
+            //Cambia il lato in cui andrà la palla
+            palla1.setspeedX(6.5);
+            palla1.setspeedY((PunteggioAI % 2 == 0) ? -6.5 : 6.5);
         }
         else if (palla1.GetX() - 20 <= 0) {
             palla1.ResetPartita();
             PunteggioUser++;
+
+            showGoText = true;
+            goTextFrames = goTextDurationFrames;
+
+            //effetti illuminazione
+            showLightEffect = 30;
+            lightRect = { 10, 10, 50, screenHeight - 20 };
+
+            //Cambia il lato in cui andrà la palla
+            palla1.setspeedX(-6.5);
+            palla1.setspeedY((PunteggioUser % 2 == 0) ? -6.5 : 6.5);
+        }
+        else {
+            showLightEffect--;
         }
 
-        BeginDrawing(); //disegna il canva dove possiamo disegnare gli oggetti
+        //Aumentare o diminuire velocità AI
+        if ((PunteggioAI + PunteggioUser == 3 || PunteggioAI + PunteggioUser == 6) && counter == 0) {
+            PaddleAI_speed += IncrementoDifficolta(PunteggioAI, PunteggioUser); 
+            counter = 1; // Blocca ulteriori incrementi finché non viene resettato
+        }
+        else if (PunteggioAI + PunteggioUser != 3 && PunteggioAI + PunteggioUser != 6) {
+            counter = 0; // Resetta il counter per il prossimo incremento
+        }
+
+        BeginDrawing(); 
         
-        ClearBackground(BLACK);
-        DrawText(TextFormat("FPS: %d", GetFPS()), 10, 10, 20, GREEN);
+        ClearBackground(cornflowerBlue);
+        DrawText(TextFormat("FPS: %d", GetFPS()), 10, 10, 20, TextColor); // Mostra gli FPS a schermo
         
         if ((CheckCollisionCircleRec(palla1.GetCD(), (float)20, PaddleGiocatore)) || (CheckCollisionCircleRec(palla1.GetCD(), (float)20, PaddleAI))){
-            palla1.InvertiVelocitaX(); //inverte la velocità della palla se c'è una collisione con un paddle
-            palla1.UpdateBall(); //aggiorna la posizione della palla per evitare sovrapposizioni 
+            palla1.InvertiVelocitaX(); // Inverte la velocità della palla se c'è una collisione con un paddle
+            palla1.UpdateBall(); // Aggiorna la posizione della palla per evitare sovrapposizioni 
         }
         //drawing objects
         palla1.DrawBall();
-        DrawRectangleRec(PaddleAI, WHITE); //Disegna Paddle giocatore 1 
-        DrawRectangleRec(PaddleGiocatore, WHITE); //Paddle giocatore 2
-        DrawLine(screenWidth / 2, 0, screenWidth / 2, screenHeight, WHITE);
-        DrawText("YOUR SIDE", (screenWidth/2) + 25, 50, 25, GREEN);
-        DrawText("CPU SIDE", ((screenWidth / 2) - (MeasureText("CPU SIDE", 25) + 25)), 50, 25, GREEN);
-        DrawText(TextFormat("%d", PunteggioUser), (screenWidth / 2) + (MeasureText("CPU SIDE", 25) / 2), 10, 30, WHITE);
-        DrawText(TextFormat("%d", PunteggioAI), (screenWidth / 2) - ((MeasureText("CPU SIDE", 25) / 2) + MeasureText("0",30)), 10, 30, WHITE);
-        EndDrawing(); //finisce il disegno
+        DrawRectangleRec(PaddleAI, PaddleColor); // Disegna Paddle giocatore 1 
+        DrawRectangleRec(PaddleGiocatore, PaddleColor); // Paddle giocatore 2
+        DrawLine(screenWidth / 2, 0, screenWidth / 2, screenHeight, BLACK);
+        DrawText("YOUR SIDE", (screenWidth/2) + 25, 50, 25, TextColor);
+        DrawText("CPU SIDE", ((screenWidth / 2) - (MeasureText("CPU SIDE", 25) + 25)), 50, 25, TextColor);
+        DrawText(TextFormat("%d", PunteggioUser), (screenWidth / 2) + (MeasureText("CPU SIDE", 25) / 2), 10, 30, BLACK);
+        DrawText(TextFormat("%d", PunteggioAI), (screenWidth / 2) - ((MeasureText("CPU SIDE", 25) / 2) + MeasureText("0",30)), 10, 30, BLACK);
+        MostraTestoGo(showGoText, goTextFrames, goTextDurationFrames, showLightEffect); //Mostrare il testo GO!
+        // Disegna l'effetto di illuminazione se necessario
+        LightEffect(lightRect, lightColor, showLightEffect, palla1);
+        EndDrawing(); 
     }
 
     // De-Initialization
@@ -139,3 +245,5 @@ int main(void)
 
     return 0;
 }
+
+//DrawText("GO!", (screenWidth / 2) - (MeasureText("GO!", 60) / 2), (screenHeight / 2) - (MeasureText("GO!", 60)), 60, BLACK);
